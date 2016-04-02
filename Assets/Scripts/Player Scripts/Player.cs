c542a8e8a27b71e4755a6538d21db604aef11e6d
Joseph Burke <joseph.burke@ul.ie>
2016-04-02 11:36:48 +0100

    player movement

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //private Vector2 move;
+    private int moveSpeed = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkMovement();
+    }
+
+    private void checkMovement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveRight();
+        }
+    }
+
+
+
+    private void moveRight()
+    {
+        transform.position += new Vector3(moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
 }
