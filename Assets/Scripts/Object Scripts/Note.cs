d3bf2c3ace97e3aabdb432526a8f71a875d7679d
Judith Hartmann <mail@judith-hartmann.net>
2016-04-02 18:47:03 +0100

    implement note collecting

--- Assets/Scripts/Object Scripts/Note.cs
+++ Assets/Scripts/Object Scripts/Note.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+
 using System.Collections;
 
 public class Note : MonoBehaviour {
 
 	public int NoteNumber = 0;
 
-	private string[] NoteText;
+	private static string[] NoteText = new string[] {
+		"Note 0 Content!",
+		"Note 1 Content!"
+	};
+
+	public GameObject TextCanvas;
+
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
@@ -18,6 +25,10 @@ public class Note : MonoBehaviour {
 	}
 
 	public void Collect () {
-		Debug.Log("Collect Note");
+		UnityEngine.UI.Text UIText = TextCanvas.GetComponentInChildren<Text>();
+		UIText.text = NoteText[NoteNumber];
+
+		TextCanvas.SetActive(true);
+		gameObject.SetActive(false);
 	}
 }
