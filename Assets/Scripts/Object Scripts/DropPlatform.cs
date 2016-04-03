34d1894faaca7605fee9d9f7ce35af653c20e6ad
Daniel Sheehan <danielsheehan.ds@gmail.com>
2016-04-03 14:08:50 +0100

    Pipes

---
+++ Assets/Scripts/Object Scripts/DropPlatform.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropPlatform : MonoBehaviour {
+
+    public GameObject player;
+    public bool onPlatform = false;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (onPlatform == true)
+        {
+            GetComponent<Rigidbody2D>().isKinematic = false;
+            anim.SetInteger("ColourChange", 1);
+
+        }
+    }
+
+        void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            onPlatform = true;
+        }
+    }
+}
