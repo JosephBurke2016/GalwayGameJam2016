885d532c53f40e0780bea114d8f1dab135780ab1
Joseph Burke <joseph.burke@ul.ie>
2016-04-06 12:05:35 +0100

    scene update - attach script

--- Assets/Scripts/Object Scripts/ElectricBox.cs
+++ Assets/Scripts/Object Scripts/ElectricBox.cs
@@ -1,21 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
 
-public class ElectricalCable : MonoBehaviour {
+public class ElectricBox : MonoBehaviour {
 
     public GameObject player;
     public Animator anim;
     public bool playerDetected = false;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         anim = player.GetComponent<Animator>();
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
 
     }
 
@@ -29,4 +30,5 @@ public class ElectricalCable : MonoBehaviour {
 
         }
     }
+
 }
